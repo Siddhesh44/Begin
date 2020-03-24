fbf23f35a8b4f7c25abf8fc15aed4e7842593257
Siddhesh <siddhesh.j@infiny.in>
2020-03-24 18:34:13 +0530

    completedTask saving/fetching problem

--- Begin/Storyboard/doneTaskTableViewController.swift
+++ Begin/Storyboard/doneTaskTableViewController.swift
@@ -16,6 +16,7 @@ class doneTaskTableViewController: UIViewController,UITableViewDelegate,UITableV
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         tableView.delegate = self
         tableView.dataSource = self
         
